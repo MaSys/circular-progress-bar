6090bd51bcceeb3e123b6fb664b77d24a5341d02
Yaser Almasri <yaser@masys.mx>
2021-03-31 23:23:27 -0600

    circular progress bar

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarApp.swift
+//  CircularProgressBar
+//
+//  Created by Yaser Almasri on 31/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
